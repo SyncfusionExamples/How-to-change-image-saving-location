251415426d0b200c56cc254ac90d16323b1b6dbd
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2023-10-27 19:55:54 +0530

    KB sample modified

--- ImageEditor_GettingStarted/ImageEditor_GettingStarted.Android/MainActivity.cs
+++ ImageEditor_GettingStarted/ImageEditor_GettingStarted.Android/MainActivity.cs
@@ -3,28 +3,26 @@
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Android.Content;
-using System.IO;
-using Android.Graphics;
 
 namespace ImageEditor_GettingStarted.Droid
 {
-    [Activity(Label = "ImageEditor_GettingStarted", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ImageEditor_GettingStarted", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         internal static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
             Instance = this;
             base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public event EventHandler<ActivityResultEventArgs> ActivityResult;
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
@@ -35,7 +33,15 @@ namespace ImageEditor_GettingStarted.Droid
                     ActivityResult(this, new ActivityResultEventArgs { Intent = data });
             }
         }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
+
     public class ActivityResultEventArgs : EventArgs
     {
         public Intent Intent
--- ImageEditor_GettingStarted/ImageEditor_GettingStarted/IDependency.cs
+++ ImageEditor_GettingStarted/ImageEditor_GettingStarted/IDependency.cs
@@ -1,19 +1,9 @@
-﻿using Syncfusion.SfImageEditor.XForms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
 
 namespace ImageEditor_GettingStarted
 {
-
     public interface IDependency
     {
         void SelectDirectory(Stream stream);
     }
 }
-
-
-
